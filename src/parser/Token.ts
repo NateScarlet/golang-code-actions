d74f75b6ed17403b1718f13f46388a341abbd04f
NateScarlet <NateScarlet@Gmail.com>
2025-01-10 14:44:03 +0800

    chore: remove unused code

--- src/parser/Token.ts
+++
@@ -1,7 +0,0 @@
-import type Comment from "./Comment";
-import type Struct from "./Struct";
-import type StructField from "./StructField";
-
-type Token = Struct | StructField | Comment;
-
-export default Token;
--- src/utils/iterateTextLines.ts
+++
@@ -1,9 +0,0 @@
-import type * as vscode from "vscode";
-
-export default function* iterateTextLines(
-  document: vscode.TextDocument
-): Iterable<vscode.TextLine> {
-  for (let i = 0; i < document.lineCount; i += 1) {
-    yield document.lineAt(i);
-  }
-}
